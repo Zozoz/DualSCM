06f9c65c6cbf1b8d045c7e269a7fcda03bca4d2e
Zozoz <zhengshiliang0@gmail.com>
2017-10-30 15:09:40 +0800

    fix a bug

--- model/sen_clf.py
+++ model/sen_clf.py
@@ -16,60 +16,45 @@ from newbie_nn.att_layer import mlp_attention_layer
 from utils.data_helper import load_w2v, batch_index, load_inputs_document_nohn
 
 
-def dual_sen(inputs_o, sen_len_o, inputs_r, sen_len_r, keep_prob1, keep_prob2):
+def lstm_sen(inputs, sen_len, keep_prob1, keep_prob2):
     print 'I am dual-sen!'
     cell = tf.contrib.rnn.LSTMCell
-    inputs_o = tf.nn.dropout(inputs_o, keep_prob=keep_prob1)
-    hiddens_o = bi_dynamic_rnn(cell, inputs_o, FLAGS.n_hidden, sen_len_o, FLAGS.max_sentence_len, 'sentence_o', 'last')
-
-    inputs_r = tf.nn.dropout(inputs_r, keep_prob=keep_prob1)
-    hiddens_r = bi_dynamic_rnn(cell, inputs_r, FLAGS.n_hidden, sen_len_r, FLAGS.max_sentence_len, 'sentence_r', 'last')
-
-    hiddens = tf.concat([hiddens_o, hiddens_r], 1)
-    logits = softmax_layer(hiddens, 4 * FLAGS.n_hidden, FLAGS.random_base, keep_prob2, FLAGS.l2_reg, FLAGS.n_class)
+    inputs = tf.nn.dropout(inputs, keep_prob=keep_prob1)
+    hiddens = bi_dynamic_rnn(cell, inputs, FLAGS.n_hidden, sen_len, FLAGS.max_sentence_len, 'sentence_o', 'last')
+    logits = softmax_layer(hiddens, 2 * FLAGS.n_hidden, FLAGS.random_base, keep_prob2, FLAGS.l2_reg, FLAGS.n_class)
     return logits
 
 
-def dual_att_sen(inputs_o, sen_len_o, inputs_r, sen_len_r, keep_prob1, keep_prob2):
+def lstm_att_sen(inputs, sen_len, keep_prob1, keep_prob2):
     print 'I am dual-att-sen!'
     cell = tf.contrib.rnn.LSTMCell
-    inputs_o = tf.nn.dropout(inputs_o, keep_prob=keep_prob1)
-    hiddens_o = bi_dynamic_rnn(cell, inputs_o, FLAGS.n_hidden, sen_len_o, FLAGS.max_sentence_len, 'sentence_o', 'all')
+    inputs = tf.nn.dropout(inputs, keep_prob=keep_prob1)
+    hiddens = bi_dynamic_rnn(cell, inputs, FLAGS.n_hidden, sen_len, FLAGS.max_sentence_len, 'sentence_o', 'all')
 
-    inputs_r = tf.nn.dropout(inputs_r, keep_prob=keep_prob1)
-    hiddens_r = bi_dynamic_rnn(cell, inputs_r, FLAGS.n_hidden, sen_len_r, FLAGS.max_sentence_len, 'sentence_r', 'all')
-
-    hiddens = tf.concat([hiddens_o, hiddens_r], 2)
-    alpha = mlp_attention_layer(hiddens, sen_len_o, 4 * FLAGS.n_hidden, FLAGS.l2_reg, FLAGS.random_base, 'sen')
-    outputs = tf.reshape(tf.matmul(alpha, hiddens), [-1, 4 * FLAGS.n_hidden])
-    logits = softmax_layer(outputs, 4 * FLAGS.n_hidden, FLAGS.random_base, keep_prob2, FLAGS.l2_reg, FLAGS.n_class)
+    alpha = mlp_attention_layer(hiddens, sen_len, 2 * FLAGS.n_hidden, FLAGS.l2_reg, FLAGS.random_base, 'sen')
+    outputs = tf.reshape(tf.matmul(alpha, hiddens), [-1, 2 * FLAGS.n_hidden])
+    logits = softmax_layer(outputs, 2 * FLAGS.n_hidden, FLAGS.random_base, keep_prob2, FLAGS.l2_reg, FLAGS.n_class)
     return logits
 
 
 def main_(_):
-    word_id_mapping_o, w2v_o = load_w2v(FLAGS.embedding_file, FLAGS.embedding_dim, True)
-    word_embedding_o = tf.constant(w2v_o, dtype=tf.float32)
-    word_id_mapping_r, w2v_r = load_w2v(FLAGS.embedding_file_r, FLAGS.embedding_dim, True)
-    word_embedding_r = tf.constant(w2v_r, dtype=tf.float32)
+    word_id_mapping, w2v = load_w2v(FLAGS.embedding_file, FLAGS.embedding_dim, True)
+    word_embedding = tf.constant(w2v, dtype=tf.float32)
 
     with tf.name_scope('inputs'):
         keep_prob1 = tf.placeholder(tf.float32)
         keep_prob2 = tf.placeholder(tf.float32)
-        x_o = tf.placeholder(tf.int32, [None, FLAGS.max_sentence_len])
-        x_r = tf.placeholder(tf.int32, [None, FLAGS.max_sentence_len])
-        len_o = tf.placeholder(tf.int32, [None])
-        len_r = tf.placeholder(tf.int32, [None])
+        x = tf.placeholder(tf.int32, [None, FLAGS.max_sentence_len])
+        sen_len = tf.placeholder(tf.int32, [None])
         y = tf.placeholder(tf.float32, [None, FLAGS.n_class])
 
-        inputs_o = tf.nn.embedding_lookup(word_embedding_o, x_o)
-        inputs_o = tf.reshape(inputs_o, [-1, FLAGS.max_sentence_len, FLAGS.embedding_dim])
-        inputs_r = tf.nn.embedding_lookup(word_embedding_r, x_r)
-        inputs_r = tf.reshape(inputs_r, [-1, FLAGS.max_sentence_len, FLAGS.embedding_dim])
+        inputs = tf.nn.embedding_lookup(word_embedding, x)
+        inputs = tf.reshape(inputs, [-1, FLAGS.max_sentence_len, FLAGS.embedding_dim])
 
         if FLAGS.method.lower() == 'att':
-            prob = dual_att_sen(inputs_o, len_o, inputs_r, len_r, keep_prob1, keep_prob2)
+            prob = lstm_att_sen(inputs, sen_len, keep_prob1, keep_prob2)
         else:
-            prob = dual_sen(inputs_o, len_o, inputs_r, len_r, keep_prob1, keep_prob2)
+            prob = lstm_sen(inputs, sen_len, keep_prob1, keep_prob2)
 
     with tf.name_scope('loss'):
         reg_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
@@ -119,33 +104,21 @@ def main_(_):
 
         tr_x, tr_y, tr_sen_len = load_inputs_document_nohn(
             FLAGS.train_file,
-            word_id_mapping_o,
+            word_id_mapping,
             FLAGS.max_sentence_len
         )
         te_x, te_y, te_sen_len = load_inputs_document_nohn(
             FLAGS.test_file,
-            word_id_mapping_o,
-            FLAGS.max_sentence_len
-        )
-        tr_x_r, tr_y_r, tr_sen_len_r = load_inputs_document_nohn(
-            FLAGS.train_file_r,
-            word_id_mapping_r,
-            FLAGS.max_sentence_len
-        )
-        te_x_r, te_y_r, te_sen_len_r = load_inputs_document_nohn(
-            FLAGS.test_file_r,
-            word_id_mapping_r,
+            word_id_mapping,
             FLAGS.max_sentence_len
         )
 
-        def get_batch_data(xo, slo, xr, slr, yy, batch_size, kp1, kp2, is_shuffle=True):
+        def get_batch_data(xo, slo, yy, batch_size, kp1, kp2, is_shuffle=True):
             for index in batch_index(len(yy), batch_size, 1, is_shuffle):
                 feed_dict = {
-                    x_o: xo[index],
-                    x_r: xr[index],
+                    x: xo[index],
                     y: yy[index],
-                    len_o: slo[index],
-                    len_r: slr[index],
+                    sen_len: slo[index],
                     keep_prob1: kp1,
                     keep_prob2: kp2,
                 }
@@ -155,14 +128,13 @@ def main_(_):
         max_prob = None
         step = 0
         for i in xrange(FLAGS.n_iter):
-            for train, _ in get_batch_data(tr_x, tr_sen_len, tr_x_r, tr_sen_len_r, tr_y,
-                                           FLAGS.batch_size, FLAGS.keep_prob1, FLAGS.keep_prob2):
+            for train, _ in get_batch_data(tr_x, tr_sen_len, tr_y, FLAGS.batch_size, FLAGS.keep_prob1, FLAGS.keep_prob2):
                 _, step = sess.run([train_op, global_step], feed_dict=train)
                 # train_summary_writer.add_summary(summary, step)
 
             acc, cost, cnt = 0., 0., 0
             p = []
-            for test, num in get_batch_data(te_x, te_sen_len, te_x_r, te_sen_len_r, te_y, 2000, 1.0, 1.0, False):
+            for test, num in get_batch_data(te_x, te_sen_len, te_y, 2000, 1.0, 1.0, False):
                 _loss, _acc, _p = sess.run([loss, accuracy_num, prob], feed_dict=test)
                 p += list(_p)
                 acc += _acc
