cce17a510f473fd9814a4fefa994d3392b83708f
Zozoz <zhengshiliang0@gmail.com>
2017-10-31 16:21:00 +0800

    fix a bug

--- error_analysis.py
+++ error_analysis.py
@@ -16,6 +16,8 @@ def split_test_to_error_and_right(test_f, prob_f, error_f=None, right_f=None):
         error_f = 'error.txt'
     if right_f is None:
         right_f = 'right.txt'
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
     ep = open(os.path.join(data_dir, error_f), 'w')
     rp = open(os.path.join(data_dir, right_f), 'w')
     for prob, line in zip(open(prob_f), open(test_f)):
