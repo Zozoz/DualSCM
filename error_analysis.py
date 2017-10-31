5d7440b5af8560078871c0dbf82489bff3559abf
Zozoz <zhengshiliang0@gmail.com>
2017-10-31 16:27:30 +0800

    fix a bug

--- error_analysis.py
+++ error_analysis.py
@@ -20,14 +20,16 @@ def split_test_to_error_and_right(test_f, prob_f, error_f=None, right_f=None):
         os.makedirs(data_dir)
     ep = open(os.path.join(data_dir, error_f), 'w')
     rp = open(os.path.join(data_dir, right_f), 'w')
+    cnt = 1
     for prob, line in zip(open(prob_f), open(test_f)):
         label = line.split('||')[0].strip()
         prob = [float(item) for item in prob.split()]
         max_index = prob.index(max(prob))
         if label_name.index(label) == max_index:
-            rp.write(line)
+            rp.write(str(cnt) + '||' + line)
         else:
-            ep.write(label_name[max_index] + '||' + line)
+            ep.write(str(cnt) + '||' + label_name[max_index] + '||' + line)
+        cnt += 1
     ep.close()
     rp.close()
 
